011f5cdd99af00181b563f011dd5abf050486b14
BumagniyPacket <bumagniypacket@yandex.ru>
2017-02-03 17:13:41 +0300

    рафак

--- ocr/classifier/predict.py
+++ ocr/classifier/predict.py
@@ -1,16 +1,15 @@
 from sklearn.externals import joblib
 from skimage.transform import resize
-from ocr.segmentation import allocate_letter
 from config import CLASSIFIER_PATH
 
 clf = joblib.load(CLASSIFIER_PATH)
 
 
 def predict(character):
-    char = allocate_letter(character)
-    char = resize(char, (20, 20))
+    # char = allocate_letter(character)
+    char = resize(character, (20, 20)).ravel().reshape(1, -1)
 
-    liter_code = int(clf.predict(char.ravel().tolist()))
+    liter_code = int(clf.predict(char))
     liter = chr(liter_code)
 
     return liter
