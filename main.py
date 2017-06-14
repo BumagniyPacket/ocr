1d10d57e8b69be5f52be6e95cd0777afb012df56
BumagniyPacket <bumagniypacket@yandex.ru>
2017-06-14 18:05:36 +0300

    Added argument parser

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import argparse
+
 from ocr.get_text import magic
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', type=str,
+                        help="Write some filename(image) for text recognizing")
+    args = parser.parse_args()
+    filename = args.filename
 
-    print(magic('img/5.png'))
-
+    text = magic(filename)
+    print(text)
